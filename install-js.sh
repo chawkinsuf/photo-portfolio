b6759a7e9a3ad8e6d1bfda6f39516e6eae1f29d8
Chris Hawkins <chawkins@tenable.com>
2018-06-12 16:39:16 -0400

    copy js deps instead

---
+++ install-js.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+mkdir -p static/js/isotope
+cp -R node_modules/isotope-layout/dist/* static/js/isotope/
+
+mkdir -p static/js/photoswipe
+cp -R node_modules/photoswipe/dist/* static/js/photoswipe/
